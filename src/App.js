6a18e738f1b139cf00f391444c37f9500ef9e164
paul-tes <paulasca8462@gmail.com>
2023-06-06 12:45:55 +0300

    update task

--- src/App.js
+++ src/App.js
@@ -15,12 +15,6 @@ function App() {
     setTasks(JSON.parse(localStorage.getItem('tasks')))
   }, [])
 
-  const addTask = (name) => {
-    setTasks(prev => {
-      return [...prev, {name: name, done: false}];
-    })
-  }
-
   const updateTaskDone = (index, status) => {
     setTasks(prev => {
       const newTasks = [...prev];
